bed36c1e9facbf1a903d9ca6afa82fff31725259
IrohasDarknesss <kaine0401mario@gmail.com>
2022-11-26 12:45:35 +0900

    move control and settings

---
+++ src/objects/projectiles/projectile.cpp
@@ -0,0 +1,13 @@
+#include "projectile.hpp"
+
+Projectile::Projectile(){
+    //ctor
+}
+
+Projectile::~Projectile(){
+    //dtor
+}
+
+float Projectile::getDepth() { return depth; }
+
+float Projectile::getDrawDepth() { return depth + Entity::DEPTH_DIFF;}
